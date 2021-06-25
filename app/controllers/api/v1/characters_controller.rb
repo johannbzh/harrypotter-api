ab04c73a0a8f37f9d2dcd5bbe23328e23c0a1b7b
johann bzaih <johann.bzaih@gmail.com>
2021-06-25 09:40:07 +0200

    :sparkles: 2nd enpoint show done

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::CharactersController < Api::V1::BaseController
   def index
     @characters = policy_scope(Character)
   end
+
+  def show
+    @character = Character.find(params[:id])
+    authorize @character
+  end
 end
--- app/policies/character_policy.rb
+++ app/policies/character_policy.rb
@@ -4,4 +4,8 @@ class CharacterPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    return true
+  end
 end
