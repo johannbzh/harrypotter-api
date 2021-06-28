63f3b4d1295a4d33e893c2a1f1fc95f46247b8d6
johann bzaih <johann.bzaih@gmail.com>
2021-06-28 17:15:40 +0200

    first attempt of scraping from letter A to D. Missing info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+puts "Creating characters..."
+
+dictio = ("A".."Z").to_a
+dictio.each do |let|
+  url = "https://www.hp-lexicon.org/character/?letter=#{let}"
+  html_file = URI.open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+  links = html_doc.css('a')
+  hrefs = links.map { |e| e.attribute('href').to_s}.uniq.sort.delete_if {|href| href.empty?}.select{|e| e.include?("https://www.hp-lexicon.org/character/")}.each do |element|
+    html_file_child = URI.open(element).read
+    html_doc_child = Nokogiri::HTML(html_file_child)
+    html_doc_child.search('.fact_box').each do |element|
+      element.search('.header').nil? ? name = "" : name = element.search('.header').text.strip
+      Character.create(name: name)
+    end
+  end
+  puts "Letter A characters have been created!"
+end
